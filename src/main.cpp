3a26699ae95364951d6ca1fd2b55f7e17f3a4beb
Mikołaj Długołęcki <mikolaj.dlug@gmail.com>
2025-03-11 00:38:17 +0100

    Set up continued

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 int main(void)
 {
-    std::cout<<"Hello world!"<<std::endl;
+    GLFWwindow* window;
+    if(!glfwInit()){
+        std::cerr<<"Can't initalize GLWF."<<std::endl;
+        return EXIT_FAILURE;
+    }
+    window = glfwCreateWindow(1280, 960, "OpenGL game", NULL, NULL);
+    if(!window){
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    GLenum err;
+    if((err = glewInit()) != GLEW_OK){
+        std::cerr<<"Cant' initalize GLEW: "<<glewGetErrorString(err)<<std::endl;
+        return EXIT_FAILURE;
+    }
+    while(!glfwWindowShouldClose(window)){
+        glClearColor(0,0.8,1,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
 
